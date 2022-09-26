598735e821629d0723948412f5e4aca328e71ac0
jiawei.liu <jiawei.liu@thoughtworks.com>
2022-09-26 17:09:01 +0800

    fix: clear main

--- src/main/java/com/tw/capability/Main.java
+++ src/main/java/com/tw/capability/Main.java
@@ -4,8 +4,5 @@ public class Main {
     public static void main(String[] args) {
         Check check = new Check();
         System.out.println(check.checkNumber(args[0]));
-        for (int i = 1; i < 20; i++) {
-            System.out.print(check.checkNumber(String.valueOf(i)) + ' ');
-        }
     }
 }
