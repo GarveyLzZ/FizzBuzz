0cf05ac0934c12251ef1afaeda77d2f83d6ce2c7
jiawei.liu <jiawei.liu@thoughtworks.com>
2022-09-29 10:31:53 +0800

    style: delete unused imports and annotation

--- src/main/java/com/tw/capability/Check.java
+++ src/main/java/com/tw/capability/Check.java
@@ -7,7 +7,6 @@ public class Check {
     private final FizzBuzz fizzBuzz = new FizzBuzz();
 
     public String checkNumber(String inputNumber) {
-//        return fizzBuzz.checkContain7(inputNumber);
         return Optional.ofNullable(
                         Optional.ofNullable(
                                         Optional.ofNullable(fizzBuzz.checkContain7(inputNumber)).
