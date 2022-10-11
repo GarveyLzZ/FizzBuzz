fc6956e29fb847c11694b7ff76634526757e7b07
jiawei.liu <jiawei.liu@thoughtworks.com>
2022-10-11 18:43:07 +0800

    style: add findFirst()

--- src/main/java/com/tw/capability/Student.java
+++ src/main/java/com/tw/capability/Student.java
@@ -2,7 +2,7 @@ package com.tw.capability;
 
 import com.tw.capability.rules.*;
 
-import java.util.stream.Collectors;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
@@ -11,23 +11,14 @@ import java.util.stream.Stream;
  */
 public class Student {
     public String getFizzBuzz(String inputNumber) {
-//        contained7 contained7 = new contained7();
-//        contained5 contained5 = new contained5();
-//        contained3 contained3 = new contained3();
-//        division3 division3 = new division3();
-//        division5 division5 = new division5();
-//        division7 division7 = new division7();
-//        System.out.println("contained7" + " " + contained7.contained(inputNumber) + " " + contained7.division(inputNumber));
-//        System.out.println("contained5" + " " + contained5.contained(inputNumber) + " " + contained5.division(inputNumber));
-//        System.out.println("contained3" + " " + contained3.contained(inputNumber) + " " + contained3.division(inputNumber));
-//        System.out.println("division3" + " " + division3.contained(inputNumber) + " " + division3.division(inputNumber));
-//        System.out.println("division5" + " " + division5.contained(inputNumber) + " " + division5.division(inputNumber));
-//        System.out.println("division7" + " " + division7.contained(inputNumber) + " " + division7.division(inputNumber));
-        String result = Stream.of(new contained7(), new contained5(), new contained3(), new Default())
+        String result = null;
+        Optional<String> firstResult = Stream.of(new contained7(), new contained5(), new contained3(), new Default())
                 .filter(strategy -> strategy.contained(inputNumber))
                 .map(strategy -> strategy.division(inputNumber))
-                .distinct()
-                .collect(Collectors.joining());
+                .findFirst();
+        if (firstResult.isPresent()){
+            result=firstResult.get();
+        }
         return "".equals(result) ? inputNumber : result;
     }
 }
